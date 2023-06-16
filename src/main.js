ec2fdc308cc86ac82c5a1418a41ebd4c28c89d6d
dhanashree <dhanichavan795@gmail.com>
2023-06-17 01:30:36 +0530

    todo1

--- src/main.js
+++ src/main.js
@@ -43,6 +43,28 @@ async function readRecord(req,res){
 
 app.get("/findAll",readRecord);
 
-app.listen(3000,()=>{
+//to do api
+async function addToDo(req,res){
+    const uri="mongodb://127.0.0.1:27017";//mongo address
+    const client = new MongoClient(uri);//mongoclient
+
+    const db=client.db("myproject");//db
+    const messageColl = db.collection("todo");//collection
+
+    //from query param
+    let inputDoc = {
+       task: req.query.task ,
+       desc: req.query.desc
+    };//data
+    await messageColl.insertOne(inputDoc);//insert data
+
+    await client.close();//close conn
+    res.json({opr:"success"});//send to remote  
+    console.log("Record Added");
+}
+
+app.get("/addtodo",addToDo);
+
+app.listen(4000,()=>{
     console.log("Server connected...")
 })
